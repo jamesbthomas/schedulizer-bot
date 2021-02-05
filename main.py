0b1135eb39dfc955e3d35e80de7cd149a93fe1a9
James Thomas <james.b.thomas@mac.com>
2021-02-05 19:21:04 +0000

    fixing gitignore

--- main.py
+++ main.py
@@ -82,4 +82,4 @@ async def on_command_error(context,error):
 
 # Call the client run method of the previously created discord client, using the value of the TOKEN key in the current directory's environment file, .env
 TOKEN = os.getenv('TOKEN')
-client.run(TOKEN)
+client.run("ODAzNjc0MTI2NjkwMTU2NTU1.YBBN2w.SnHlDdbIBFjDvV8f69izeDQy-xc")
