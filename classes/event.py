9c646e8ce5fa1c5a58e3631dbb7c95b0fcc60c08
James Thomas <james.b.thomas@mac.com>
2021-02-08 21:42:12 +0000

    event superclass constructor and tests

--- classes/event.py
+++ classes/event.py
@@ -1,1 +1,23 @@
-# Defines the Event class
+# Defines the Event class and associated subclasses
+import datetime
+
+class Event(object):
+
+  def __init__(self,date: datetime.datetime,recurring: bool,description: str):
+    if isinstance(date,datetime.datetime):
+      self.date = date
+    else:
+      raise TypeError("\'date\' must be of type datetime")
+    if isinstance(recurring,bool):
+      self.recurring = recurring
+    else:
+      raise TypeError("\'recurring\' must be of type bool")
+    if isinstance(description,str):
+      self.description = description
+    else:
+      raise TypeError("\'description\' must be of type str")
+
+class Raid(Event):
+  # Used to signifiy a raid (for auto-cooking the roster)
+  def __init__(self,date: datetime,recurring: bool,description: str):
+    return
--- tests/test_event.py
+++ tests/test_event.py
@@ -1,1 +1,52 @@
 # Test-Driven Development for the Event class
+import pytest
+import sys
+import os
+import datetime
+project_root = os.path.split(os.path.dirname(__file__))[0]
+print(project_root)
+sys.path.append(os.path.join(project_root,"classes"))
+
+# Import module for testing
+import event
+
+def test_makeEvent():
+  """
+  GIVEN Event constructor and appropriate inputs
+  WHEN the method is called
+  THEN create and return a correctly structured Event object
+  """
+  # Test expected use
+  ## Datetime constructor: year, month, day, hour, minute, second
+  d = datetime.datetime(1970,1,1,0,0,0)
+  recurring = event.Event(d,True,"recurring event")
+  once = event.Event(d,False,"event that occurs once")
+
+  assert recurring.date == d
+  assert recurring.recurring == True
+  assert recurring.description == "recurring event"
+  assert once.date == d
+  assert once.recurring == False
+  assert once.description == "event that occurs once"
+  # Test bad datatypes
+  ## bad date
+  with pytest.raises(TypeError):
+    badDate = event.Event("1970-01-01 00:00:00",True,"has a bad date")
+    print(badDate.date)
+    print(type(badDate.date))
+  ## bad recurring
+  with pytest.raises(TypeError):
+    badRecurring = event.Event(d,"true","has bad recurring")
+  ## bad description
+  with pytest.raises(TypeError):
+    badDescription = event.Event(d,True,123)
+
+def test_makeRaid():
+  """
+  GIVEN Raid constructor and appropriate inputs
+  WHEN the method is called
+  THEN create and return a correctly structured Event object
+  """
+
+  assert True
+  return
